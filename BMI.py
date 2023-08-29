1b67a999892bfb234f6ffd881d0b840d131ef1f8
pfrmn <pawelfurman@me.com>
2023-08-29 10:50:57 +0200

    classification and rounding output result

--- BMI.py
+++ BMI.py
@@ -1,4 +1,4 @@
-#BMI counter
+#BMI counter © pfrmn
 
 weight = input("Podaj swoją wagę w Kg:")
 height = input("Podaj swoją wysokość w m.cm:")
@@ -6,6 +6,26 @@ height = input("Podaj swoją wysokość w m.cm:")
 weightInt = (float(weight))
 heightInt = (float(height))
 BMI = weightInt / (heightInt * heightInt)
-BMIoutput = (str(BMI))
+BMIround = (round(BMI, 2))
+BMIoutput = (str(BMIround))
 
-print("Twoje BMI to:" + BMIprint)
+if BMI >= 40:
+    print("Otyłość III stopnia, BMI wynosi: " + BMIoutput)
+elif 35 <= BMI < 40:
+    print("Otyłość II stopnia, BMI wynosi: " + BMIoutput)
+elif 30 <= BMI < 35:
+    print("Otyłość I stopnia, BMI wynosi: " + BMIoutput)
+elif 27.5 <= BMI < 30:
+    print("Nadwaga wysoki przedział, BMI wynosi: " + BMIoutput)
+elif 25 <= BMI < 27.5:
+    print("Nadwaga niski przedział, BMI wynosi: " + BMIoutput)
+elif 23 <= BMI < 25:
+    print("Norma wysoki przedział, BMI wynosi: " + BMIoutput)         
+elif 18.50 <= BMI < 23:
+    print("Norma niski przedział, BMI wynosi: " + BMIoutput)
+elif 17 <= BMI < 18.50:
+    print("Niedowaga, BMI wynosi: " + BMIoutput)
+elif 16 <= BMI < 17:
+    print("Wychudzenie, BMI wynosi: " + BMIoutput)                  
+else:
+    print("Wygłodzenie, BMI wynosi" + BMIoutput)
